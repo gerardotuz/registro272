3b0a3d9e23b48e4fb0f3edd08877e9bc55739245
gerardotuz <82822594+gerardotuz@users.noreply.github.com>
2026-01-21 16:39:52 -0500

    Update paraescolar.model.js

--- backend/models/paraescolar.model.js
+++ backend/models/paraescolar.model.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 
 const ParaescolarSchema = new mongoose.Schema({
   numero_control: { type: String, required: true, unique: true },
-  nombres: String,
-  primer_apellido: String,
-  segundo_apellido: String,
+  curp: String,
+  nombre: String,
+  grado: String,
   grupo: String,
-  turno: String,
   paraescolar: { type: String, default: null },
   fecha_registro: Date,
   bloqueado: { type: Boolean, default: false }
