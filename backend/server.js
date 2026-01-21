5717d57d46fd0b5c361307185b1242605c06dcdd
gerardotuz <82822594+gerardotuz@users.noreply.github.com>
2026-01-21 09:12:40 -0500

    Import Paraescolar model in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const Paraescolar = require("./models/paraescolar.model");
+
 require('dotenv').config();
 
 const app = express();
