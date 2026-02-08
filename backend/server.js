bf070ed55c351023c771b0fc4e28c596b8888dc6
gerardotuz <82822594+gerardotuz@users.noreply.github.com>
2026-02-08 17:07:22 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -69,6 +69,8 @@ app.use('/api', require('./routers/auth.js'));
 app.use('/api', require('./routers/grupo.js'));
 app.use('/api/dashboard', require('./routers/dashboard'));
 app.use('/api', require('./routers/padron.js'));
+app.use('/api/superadmin', require('./routers/superadmin'));
+
 
 
 /* =========================
